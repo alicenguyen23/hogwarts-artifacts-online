73d55e7fea2b30839d6904881348916b0bfb9f98
Alice Nguyen <nguyet.nguyen@tcu.edu>
2024-04-12 14:35:54 -0500

    User Crud Done! Close #4

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactService.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactService.java
@@ -21,7 +21,7 @@ public class ArtifactService {
 
     public Artifact findById(String artifactId) {
         return this.artifactRepository.findById(artifactId)
-                .orElseThrow(() -> new ObjectNotFoundException(artifactId));
+                .orElseThrow(() -> new ObjectNotFoundException("artfact", artifactId));
     }
     public List<Artifact> findAll() {
 
@@ -42,12 +42,12 @@ public class ArtifactService {
 
                    return this.artifactRepository.save(oldArtifact);
                 })
-                .orElseThrow(() -> new ObjectNotFoundException(artifactId));
+                .orElseThrow(() -> new ObjectNotFoundException("artfact", artifactId));
     }
 
     public void delete(String artifactId) {
         this.artifactRepository.findById(artifactId)
-                .orElseThrow(() -> new ObjectNotFoundException(artifactId));
+                .orElseThrow(() -> new ObjectNotFoundException("artfact", artifactId));
         this.artifactRepository.deleteById(artifactId);
     }
 }
